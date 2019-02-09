1ebf40c1d9980bb8af7e9a25b3514fa968d7aed0
Kyler <Krenneker16@gmail.com>
2019-02-09 17:02:06 -0500

    added functionality all the way up to next question

--- main.js
+++ main.js
@@ -20,29 +20,8 @@ const QUESTIONS = [
       'Seville'
     ],
     correctAsnwer: 'Madrid',
-  },
+  }
 
-  {
-    question: 'What is the capital of Spain?',
-    answers: [
-      'Barcelona',
-      'Granada',
-      'Madrid',
-      'Seville'
-    ],
-    correctAsnwer: 'Madrid',
-  },
-
-  {
-    question: 'What is the capital of Spain?',
-    answers: [
-      'Barcelona',
-      'Granada',
-      'Madrid',
-      'Seville'
-    ],
-    correctAsnwer: 'Madrid',
-  },
 ];
 
 //The COUNTER object is used to store the current score and question number
@@ -52,23 +31,37 @@ const QUESTIONS = [
 //   questionNumber: 0
 // }
 
-let questionIndex = 0;
-let score = 0;
-
+const INDEX = {
+  questionIndex: 0
+}
 const STORE = {
-    question: QUESTIONS[questionIndex].question,
-    answers: QUESTIONS[questionIndex].answers,
-    correctAsnwer: QUESTIONS[questionIndex].correctAsnwer,
+    question: QUESTIONS[INDEX.questionIndex].question,
+    answers: QUESTIONS[INDEX.questionIndex].answers,
+    correctAsnwer: QUESTIONS[INDEX.questionIndex].correctAsnwer,
     score: 0,
-    questionNumber: 1,
-    view: 'initialView'
+    questionNumber: 0,
+    // view: 'initialView'
 };
-console.log(STORE.answers);
+// console.log(STORE.answers);
 
+function generateIntroView(){
+  return `<div class="js-quiz-start">
+  <h1>Country Quiz</h1>
+  <p>Let's test your knowledge on country's capitals!</p>
+  <button type="button" name="button" class="startButton">Start Quiz!</button>
+</div>`
+}
 
 function render() {
-  renderQuestionAnswerText();
- 
+  if(STORE.questionNumber === 0){
+    $('.questionAnswerForm').html(generateIntroView());
+    STORE.questionNumber++;
+  }
+  else if(STORE.questionNumber < QUESTIONS.length){
+    renderQuestionAnswerText();
+    handleAnswerSubmitted();
+  }
+
 }
 
 
@@ -101,8 +94,7 @@ function loadQuesitonToSTORE(){
 //check to see if the questionNumber is less than or equal to the QUESTIONS.length
 //if it is generate the questino based on the questionNumber 
 //else render the quiz results page
-function generateQuestion(){
-  console.log(typeof answers);
+function generateQuestion(obj){
   return `<section class="js-answer-template">
   <form>
     <fieldset>
@@ -136,7 +128,7 @@ function generateQuestion(){
 
 //render the question and answers and insert that HTML into the DOM
 function renderQuestionAnswerText(){
-  $('.questionAnswerForm').html(generateQuestion());
+  $('.questionAnswerForm').html(generateQuestion(STORE));
 }
 
 // on submission of answer check to see if the answer submitted is === to the answer in the STORE
@@ -146,20 +138,45 @@ function handleAnswerSubmitted(){
     // Retrieve answer identifier of user-checked radio button
     // Perform check: User answer === Correct answer?
     // Update STORE and render appropriate section
+    console.log('handleanswer is rtunning');
+    $('form').on('submit', function(event){
+      event.preventDefault();
+      let selected = $('input:checked');
+      let answer = selected.val();
+      if(answer === STORE.correctAsnwer){
+        $('.questionAnswerForm').html(generateUserFeedbackCorrect());
+        score++;
+      }
+      else{
+        $('.questionAnswerForm').html(generateUserFeedbackIncorrect());
+      }
+      STORE.questionNumber++;
+      console.log(STORE.question);
+    })
+    
+
 
     //get the value of the answer selected and assign that to a variable
     //if the the value of the answer selected is equal to the correctAnswer
-        renderUserFeedback(generateUserFeedbackCorrect());
+        // renderUserFeedback(generateUserFeedbackCorrect());
         //and increment the score and apply that to the container in the header that is keeping score
     //else
-        renderUserFeedback(generateUserFeedbackIncorrect());
+        // renderUserFeedback(generateUserFeedbackIncorrect());
 }
 
 //form and return an html template for if the user gets the answer correct 
-function generateUserFeedbackCorrect(){}
-
+function generateUserFeedbackCorrect(){
+  return `<section class="js-feedback-correct">
+  <h2>You are right!</h2>
+  <button class='js-next-btn' type='submit' name='next-question'>Next question!</button> </section>`;
+}
 //form and return an html template for if the user gets the answer incorrect
-function generateUserFeedbackIncorrect(){}
+function generateUserFeedbackIncorrect(){
+  return `<section class='js-feedback-incorrect'> <h2>You are wrong!</h2>
+      <p>The correct answer is ${STORE.correctAsnwer}</p>
+      <button class='js-next-btn' type='submit' name='next-question'>Next question!</button>
+    </section>`;
+}
 
 //depending on the answer submitted the correct or incorrect feedback html will be loaded
 //in the DOM, the next question will be loaded into the store
@@ -171,19 +188,32 @@ function renderNextQuestion(){
   //listen for a click event on the next button
   //increment the COUNTER.questionNumber
   //and:
-  if(COUNTER.questionNumber < QUESTIONS.length){
-    loadQuesitonToSTORE();
-    renderQuestionAnswerText();
-    handleAnswerSubmitted();
-  }
-  else{
-    renderQuizResults();
-
-  }
+  $('.questionAnswerForm').on('click', '.js-next-btn', function(event){
+    
+      INDEX.questionIndex++;
+      console.log(INDEX.questionIndex);
+      console.log(QUESTIONS[INDEX.questionIndex].question);
+      console.log(generateQuestion());
+      // loadQuesitonToSTORE();
+      // renderQuestionAnswerText();
+      render();
+  })
+  
 }
 
 //this will generate the html for the results page
-function generateQuizResults(){}
+function generateQuizResultsPass(){
+  return `<section class="js-quiz-results">
+        <h2>Great job! You know your capitals!</h2>
+        <button type="submit" name="restart-quiz">Restart Quiz</button></section>`;
+}
+
+function generateQuizResultsFailed(){
+  return `<section> <h2>Oh no! You did not get enough answers correct.</h2>
+  <p>Study up on your capitals and try again!</p>
+  <button type="submit" name="restart-quiz"> Restart Quiz</button>
+  </section>`;
+}
 
 //This will load the quiz results page into the DOM and call the restartQuiz function so the 
 //user can restart the quiz if desired
@@ -195,11 +225,10 @@ function restartQuiz(){
 }
 
 function creatQuiz(){
+  render();
   startQuizOnClick();
-  //renderQuestionAnswerText();
-  handleAnswerSubmitted();
+  // handleAnswerSubmitted();
   renderNextQuestion();
-  //render();
 }
 
 $(creatQuiz);
